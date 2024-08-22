ee65b80f8508e3d0923ae6e174a93bfeb9a972b5
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-08-22 21:24:58 +0530

    initial commit

---
+++ src/main/java/com/authors/records/AuthorsRecord/AuthorsRecordApplication.java
@@ -0,0 +1,13 @@
+package com.authors.records.AuthorsRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorsRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorsRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authors/records/AuthorsRecord/AuthorsRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authors.records.AuthorsRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorsRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
